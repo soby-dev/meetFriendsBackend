96867cb5cd996f484511a715d6057bcd667b03f8
Sobulachi Chinda <sobychinda@gmail.com>
2022-10-28 10:00:52 +0100

    changed allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['meetfriendsbackend.herokuapp.com' ,'127.0.0.1', 'localhost', 'meetupfriends.herokuapp.com', 'www.meetupfriends.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
