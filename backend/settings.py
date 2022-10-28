3f14668c32f2e18683a4f101be9dd114ef87d84b
Sobulachi Chinda <sobychinda@gmail.com>
2022-10-28 06:37:18 +0100

    changed runtime.txt

--- backend/settings.py
+++ backend/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mosrene.herokuapp.com', 'mosrene.com', 'www.mosrene.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'meetupfriends.herokuapp.com', 'www.meetupfriends.herokuapp.com']
 
 
 # Application definition
